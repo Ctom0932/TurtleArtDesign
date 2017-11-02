47f1ba9e7a84bc7d39d5f6f0736477e693f21f86
Ctom0932 <33285333+Ctom0932@users.noreply.github.com>
2017-11-02 08:28:48 -0400

    Add files via upload

---
+++ Design #2.py
@@ -0,0 +1,43 @@
+import turtle
+from myShape import *
+from random import randint
+turtle.colormode(255)
+turtle.bgcolor("black")
+
+bob=turtle.Turtle()
+bob.speed(0)
+turtle.tracer(0)
+
+
+for times in range(606):
+    bob.color(225,0,0)
+    bob.width(10)
+    move(bob,-4,5)
+    bob.circle(300000000)
+    bob.right(25)
+
+for times in range(35):
+    bob.color(0,0,255)
+    bob.circle(200)
+    bob.right(55)
+
+    
+
+
+
+    
+
+
+    
+
+
+    
+    
+
+
+
+
+    
+
+   
+
---
+++ myShape.py
@@ -0,0 +1,32 @@
+def square( t, distance ):
+    for times in range(4):
+        t.forward(distance)
+        t.left(90)
+def triangle( t, distance ):
+    for times in range(3):
+        t.forward(100)
+        t.left(120)
+
+def pentagon(t, distance ):
+    angle = 360/5
+    for times in range(5):
+        t.forward(distance)
+        t.left(angle)
+
+        
+def polygon(t,distance,side):
+    angle=360/side
+    t.begin_fill()
+    for times in range(side):
+        t.forward(distance)
+        t.left(angle)
+    t.end_fill()
+
+def move (t, x, y):
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+
+
+
+
